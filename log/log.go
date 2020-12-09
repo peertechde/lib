cc816436008b923c1b513a809441127f5f8b426c
Marcel Kirschner <mk@peertech.de>
2020-12-09 07:56:14 +0100

    add log package

---
+++ log/log.go
@@ -0,0 +1,24 @@
+package log
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+// Logger is the default base logger
+var Logger = defaultLogger()
+
+func defaultLogger() *logrus.Logger {
+	logger := logrus.New()
+	logger.Formatter = &logrus.TextFormatter{
+		DisableTimestamp: true,
+		DisableColors:    true,
+		FullTimestamp:    true,
+	}
+	logger.SetLevel(logrus.InfoLevel)
+	return logger
+}
+
+// SetLogLevel sets the log level on Logger
+func SetLogLevel(level logrus.Level) {
+	Logger.SetLevel(level)
+}
